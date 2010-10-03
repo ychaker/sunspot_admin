896e44c442df276de9579ef319361b3ef0f5bb29
Youssef Chaker <ychaker@opensourceconnections.com>
2010-10-03 12:07:37 -0400

    adding more meaningful comments

--- lib/sunspot_admin/sunspot_admin.rb
+++ lib/sunspot_admin/sunspot_admin.rb
@@ -4,12 +4,24 @@ module SunspotAdmin
   # TODO: figure out a way to remove old searchable block
   # TODO: or user Sunspot.setup instead of the searchable block
   # Currently, removed fields will be indexed but omitted from searches through the search controller
+  # klasses => { :User => { :string => ['name', 'initials'] }, :Sprint => { :string => ['title', 'theme'], :integer => ['number_of_days'] }
   def self.make_searchable(klasses = {})
-    klasses.each_pair do
+    klasses.each_pair do # :User => { :string => ['name', 'initials'] }
       |klass, hash|
+      # klass = User
+      # hash = { :string => ['name', 'initials'] }
+      # Object.const_get(klass) => User
+      # User.class_eval
       Object.const_get(klass).class_eval do
+        # searchable do
+        #   text :name
+        #   text :initials
+        # end
         searchable do
+          # hash = { :string => ['name', 'initials'] }
           hash.each_pair do |type, items|
+            # type = :string
+            # items = ['name', 'initials']
             # TODO: Use dynamic type instead of just 'string'
             # example: send type, items.collect { |each| each.field.to_sym }
             items.each do |item|
